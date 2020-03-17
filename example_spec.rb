3175e75c07b028ed9593f0657ebf854a0fe8ee1d
yc yap <yowchun93@gmail.com>
2020-03-17 22:28:03 +0800

    Add expect assertion syntax

--- example_spec.rb
+++ example_spec.rb
@@ -1,14 +1,13 @@
 require_relative 'runner'
 
-describe "some examples" do
-  it "can pass" do
-    (1 + 1).should == 2
+describe "expectations" do
+  it "can expect values" do
+    expect(1 + 1).to eq(2)
   end
 
-  it "can fail" do
-    (1 + 1).should == 3
-  end
-
-  it "can pass after failling" do
+  it "can expect exceptions" do
+    expect do
+      raise ArgumentError.new
+    end.to raise_error(ArgumentError)
   end
 end
--- runner.rb
+++ runner.rb
@@ -3,6 +3,28 @@ def describe(description, &block)
   block.call
 end
 
+def expect(actual=nil, &block)
+  Actual.new(actual || block)
+end
+
+def eq(expected)
+  Expectations::Equal.new(expected)
+end
+
+def raise_error(exception_class)
+  Expectations::Error.new(exception_class)
+end
+
+class Actual
+  def initialize(actual)
+    @actual = actual
+  end
+
+  def to(expectation)
+    expectation.run(@actual)
+  end
+end
+
 GREEN = "\e[32m"
 RED   = "\e[31m"
 RESET = "\e[0m"
@@ -14,6 +36,11 @@ def it(description, &block)
     puts " #{GREEN}(ok)#{RESET}"
   rescue Exception => e
     puts " #{RED}(fail)#{RESET}"
+    puts [
+      "#{RED}* Backtrace: #{RESET}",
+      e.backtrace.reverse.map { |line| "#{RED}|#{RESET} #{line}" },
+      "#{RED}* #{e} #{RESET}",
+    ].flatten.map { |line| "\t#{line}" }.join("\n")
   end
 end
 
@@ -32,6 +59,43 @@ class ComparisonAssertion
     unless @actual == expected
       raise AssertionError.new(
         "Expected #{expected.inspect} but got #{@actual.inspect}")
+    end
+  end
+end
+
+class Expectations
+  class Equal
+    def initialize(expected)
+      @expected = expected
+    end
+
+    def run(actual)
+      unless actual == @expected
+        raise AssertionError.new(
+          "Expected #{@expected.inspect} but got #{@actual.inspect}")
+      end
+    end
+  end
+
+  class Error
+    def initialize(exception_class)
+      @exception_class = exception_class
+    end
+
+    def run(actual_block)
+      begin
+        actual_block.call
+      rescue @exception_class
+        return
+      rescue StandardError => e
+        raise AssertionError.new(
+          format('Expected to see error %s, but saw %s',
+            @exception_class.inspect,
+            e.inspect))
+      end
+      raise AssertionError.new(
+        format("Expected to see error %s, but got nothing",
+          @exception_class.inspect))
     end
   end
 end
